f54358825c6b401a38b919bb5fc9d81da3e69e0d
Melek lassoued <melek@tekand.me>
2023-05-13 23:44:58 +0100

    feat: :sparkles: setup plop config

---
+++ settings/component/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  description: "Creates a new component",
+  prompts: [
+    {
+      type: "list",
+      name: "componentType",
+      message: "what component do you want to create ?",
+      choices: ["View", "Common", "Layout"],
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "what is the name of the component",
+    },
+  ],
+  actions: [
+    {
+      index: "add",
+      path: "../src/components/{{componentType}}/{{pasacalCase name}}.js",
+      templateFile: "templates/component/index.js.hbs",
+    },
+  ],
+};
